dcee2f0bf0613bb3389f675205bb9f3fc6691561
gto1234 <vladimiryar33@mail.ru>
2018-11-22 16:53:28 +0300

    Added managing elements

--- app/src/main/java/com/example/vkazancev/smartphoneprint/MainActivity.java
+++ app/src/main/java/com/example/vkazancev/smartphoneprint/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vkazancev.smartphoneprint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Load an existing file
+//        WebView webViewer = (WebView) findViewById(R.id.WebData);
+//
+//
+//        String htmlDocument = "<html><body><h1>Test Content</h1><p>Testing, " +
+//                "testing, testing...</p></body></html>";
+//        webViewer.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+
+
+
     }
 }
