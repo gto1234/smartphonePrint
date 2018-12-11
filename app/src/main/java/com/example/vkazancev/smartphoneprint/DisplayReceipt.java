40f4ddc03a1705c9e5ff7b69eb5d278968bb6478
gto1234 <vladimiryar33@mail.ru>
2018-12-11 17:06:31 +0300

    Added second activity

---
+++ app/src/main/java/com/example/vkazancev/smartphoneprint/DisplayReceipt.java
@@ -0,0 +1,20 @@
+package com.example.vkazancev.smartphoneprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayReceipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_receipt);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+}
--- app/src/main/java/com/example/vkazancev/smartphoneprint/MainActivity.java
+++ app/src/main/java/com/example/vkazancev/smartphoneprint/MainActivity.java
@@ -2,24 +2,56 @@ package com.example.vkazancev.smartphoneprint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.content.Intent;
+
+
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] aTransactionNames = {
+        "Sale",
+        "Refund",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Load an existing file
-//        WebView webViewer = (WebView) findViewById(R.id.WebData);
-//
-//
-//        String htmlDocument = "<html><body><h1>Test Content</h1><p>Testing, " +
-//                "testing, testing...</p></body></html>";
-//        webViewer.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+        //Filling spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, aTransactionNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        Spinner spinner = (Spinner) findViewById(R.id.TransactionNameValue);
+        spinner.setAdapter(adapter);
+
+
+//        //Setting button click handler
+//        Button btnReceiptGenerate = (Button)findViewById(R.id.BGenerate);
+//        OnClickListener OnReceiptGenerateClick = new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        };
 
 
     }
+
+    public void onGenerateButtonClick() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // Получаем текстовое поле в текущей Activity
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        // Получае текст данного текстового поля
+        String message = editText.getText().toString();
+        // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
+        // второй параметр - значение этого объекта
+        intent.putExtra(EXTRA_MESSAGE, message);
+        // запуск activity
+        startActivity(intent);
+    }
+
 }
